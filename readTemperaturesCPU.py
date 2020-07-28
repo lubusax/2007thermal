7820c979000624ff3d31efd2bf4bb9baa52e19f1
lubusax <lu.bu.sax@google.com>
2020-07-28 22:19:08 +0200

    autosave

--- readTemperaturesCPU.py
+++ readTemperaturesCPU.py
@@ -1,6 +1,7 @@
+# https://www.kernel.org/doc/Documentation/thermal/sysfs-api.txt
 
 for i in range(0,9):
 
   with open("/sys/class/thermal/thermal_zone"+str(i)+"/temp") as file:
     data=file.read()
-    print("thermal_zone"+str(i)+" :", int(data)/1000, "°C")
+    print("Current temperature of thermal_zone"+str(i)+" :", int(data)/1000, "°C")
